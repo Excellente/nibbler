25873ae31850f236ca999ea7a1af098100d39e6d
Excellent MSIMANGO <emsimang@e5r4p3.jnb.42.fr>
2017-06-21 16:17:19 +0200

    all

---
+++ src/game.hpp
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   game.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: emsimang <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/06/21 11:24:39 by emsimang          #+#    #+#             */
+/*   Updated: 2017/06/21 11:34:05 by emsimang         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GAME_H
+# define GAME_H
+
+# include <OpenGL/glu.h>
+# include <GLUT/glut.h>
+# include <OpenGl/gl.h>
+
+void drawGrid();
+void initGrid(int x, int y);
+#endif
---
+++ src/opengl.cpp
@@ -0,0 +1,74 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   opengl.cpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: emsimang <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/06/21 11:33:57 by emsimang          #+#    #+#             */
+/*   Updated: 2017/06/21 11:34:01 by emsimang         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "game.hpp"
+# define COLUMN 40
+# define ROW 40
+# define FPS 10
+
+void	init();
+void	display_callback();
+void	timer_callback(int val);
+void	reshape_callback(int w, int h);
+
+int main(int argc, char **argv)
+{
+	//initialize glut, before using the framework
+	glutInit(&argc, argv);
+
+	//using double buffer so as to buffer updates on the windows
+	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);
+	glutInitWindowSize(500, 500);
+	glutCreateWindow("SNAKE");
+	glutDisplayFunc(display_callback);
+	//called whenever the get created, window is resized or moved.
+	glutReshapeFunc(reshape_callback);
+
+	//registers a callback that
+	//executes everytime x amount of mses has passed
+	glutTimerFunc(0, timer_callback, 0);
+	init();
+	//calls all the necessary callbacks
+	glutMainLoop();
+	return (0);
+}
+
+void	init()
+{
+	glClearColor(0.0, 0.0, 0.0, 1.0);
+	initGrid(COLUMN, ROW);
+}
+
+void	timer_callback(int)
+{
+	//calls the display function everytime
+	glutPostRedisplay();
+	glutTimerFunc(100 / FPS, timer_callback, 0);
+}
+
+void	reshape_callback(int w, int h)
+{
+	glViewport(0, 0, (GLsizei)w, (GLsizei)h);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glOrtho(0.0, COLUMN, 0.0, ROW, -1.0, 1.0);
+	glMatrixMode(GL_MODELVIEW);
+}
+
+void	display_callback()
+{
+	//updates or resets the color buffer
+	glClear(GL_COLOR_BUFFER_BIT);
+	//draw a grid before swapping display buffers.
+	drawGrid();
+	glutSwapBuffers();
+}
